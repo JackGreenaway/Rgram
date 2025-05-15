b392757475e0f737c6b2cac08173cd065747de37
Jack <jackg18@hotmail.co.uk>
2025-05-15 21:39:43 +0100

    removed numpy dep

--- src/rgram/smoothing.py
+++ src/rgram/smoothing.py
@@ -1,26 +1,4 @@
 import polars as pl
-import numpy as np
-from numpy.typing import ArrayLike
-
-
-def silverman_rot(y: pl.Series | ArrayLike) -> float:
-    """
-    Calculate the bandwidth using Silverman's rule of thumb.
-
-    Parameters
-    ----------
-    y : pl.Series or ArrayLike
-        Input array of data points.
-
-    Returns
-    -------
-    float
-        The calculated bandwidth based on the input data.
-    """
-    std = y.std()
-    iqr = np.subtract(*np.quantile(y, [0.75, 0.25])) / 1.34
-
-    return 0.9 * min(std, iqr) * (y.shape[0] ** (-1 / 5))
 
 
 def kernel_smoothing(
